14044be05b370ebf125b2546e46026c961bf0526
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-05-09 18:03:13 +0600

    sendData Done

--- app.js
+++ app.js
@@ -7,12 +7,12 @@
 const getButton = document.getElementById('get-button')
 const sendButton = document.getElementById('send-button')
 
-const sendRequest = function (method, url) {
+const sendRequest = function (method, url, data) {
     const promise = new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest()
         xhr.responseType = 'json'
         xhr.open(method, url)
-        xhr.send()
+        xhr.send(data)
 
         xhr.onload = function () {
             resolve(xhr.response)
@@ -29,7 +29,19 @@ const getData = function () {
     )
 }
 
-const sendData = function () {}
+const sendData = function () {
+    sendRequest(
+        'GET',
+        'https://jsonplaceholder.typicode.com/posts',
+        JSON.stringify({
+            title: 'foo',
+            body: 'bar',
+            userId: 1,
+        })
+    ).then((responseData) => {
+        console.log(responseData)
+    })
+}
 
 getButton.addEventListener('click', getData)
 sendButton.addEventListener('click', sendData)
